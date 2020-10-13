15793063f33684944100b2c0413340be017c58d0
Nicat <iamnicathaciyev13@gmail.com>
2020-10-14 01:26:23 +0400

    I create menu for update fragment

--- ToDoApp/app/src/main/java/com/nicathaciyev/todoapp/fragments/update/UpdateFragment.kt
+++ ToDoApp/app/src/main/java/com/nicathaciyev/todoapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.nicathaciyev.todoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.nicathaciyev.todoapp.R
 
 
@@ -15,8 +13,15 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Set menu
+        setHasOptionsMenu(true)
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
 }
