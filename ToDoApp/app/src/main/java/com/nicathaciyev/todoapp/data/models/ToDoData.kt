38bdc0c7e0749b6b50d14b62528c9b362cb50ccc
Nicat <iamnicathaciyev13@gmail.com>
2020-10-14 22:28:19 +0400

    I create data class which include four parameters

---
+++ ToDoApp/app/src/main/java/com/nicathaciyev/todoapp/data/models/ToDoData.kt
@@ -0,0 +1,15 @@
+package com.nicathaciyev.todoapp.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+
+
+)
