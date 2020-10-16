fe17907a35166ee6084b60e4d3faa578067a1742
Nicat <iamnicathaciyev13@gmail.com>
2020-10-16 13:06:14 +0400

    I add Parcelize annotation

--- ToDoApp/app/src/main/java/com/nicathaciyev/todoapp/data/models/ToDoData.kt
+++ ToDoApp/app/src/main/java/com/nicathaciyev/todoapp/data/models/ToDoData.kt
@@ -1,9 +1,12 @@
 package com.nicathaciyev.todoapp.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "todo_table")
+@Parcelize
 data class ToDoData(
     @PrimaryKey(autoGenerate = true)
     var id: Int,
@@ -12,4 +15,4 @@ data class ToDoData(
     var description: String
 
 
-)
+) : Parcelable
